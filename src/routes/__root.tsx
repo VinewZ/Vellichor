2532d046d32374b523c008c2e4beb53761bd737a
VinewZ <vinewalves@hotmail.com>
2026-09-11 18:26:22 -0300

    chore: add favicon and link it in root document

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,6 +33,11 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				rel: "stylesheet",
 				href: appCss,
 			},
+      {
+        rel: "icon",
+        type: "image/x-svg",
+        href: "favicon.svg"
+      }
 		],
 	}),
 	shellComponent: RootDocument,
