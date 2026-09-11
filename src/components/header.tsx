5ecfe47514e99313822b567e322e70c1f46697be
VinewZ <vinewalves@hotmail.com>
2026-09-11 18:26:50 -0300

    style: responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { ThemeSelector } from "./theme-selector";
 
 export function Header() {
 	return (
-		<header className="mb-12 flex items-center justify-between border-b bg-background p-8 dark:bg-secondary-background">
+		<header className="mb-12 flex flex-col gap-5 md:flex-row md:text-left md:justify-between items-center justify-center text-center border-b bg-background p-8 dark:bg-secondary-background">
 			<h1 className="font-bold text-4xl uppercase">
 				Vellichor
 				<br />
